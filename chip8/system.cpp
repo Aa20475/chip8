0e536f447743fd24f9ace4c28f6cd6882cb8e1b3
aa20475 <aa20475@gmail.com>
2023-09-13 18:28:47 -0400

    1NNN, 00E0, 00EE, 2NNN working.

--- chip8/system.cpp
+++ chip8/system.cpp
@@ -4,7 +4,7 @@
 #include<string>
 #include<stdexcept>
 
-System::System() : delay_timer(0), sound_timer(0), pc(NULL), i_register(NULL) {
+System::System() : delay_timer(0), sound_timer(0), pc(0), i_register(NULL), rom_loaded(false) {
 	std::cout << "Initializing System... " << std::endl;
 	memory = (uint8_t*)malloc(4096);
 	i_register = malloc(2);
@@ -44,8 +44,39 @@ void System::write_to_memory(uint16_t address, uint8_t value) {
 	*(memory + address) = value;
 }
 
+uint8_t System::read_from_memory(uint16_t address) {
+	if (address >> 12)throw std::invalid_argument("Address needs to be 12-bit");
+	return *(memory + address);
+}
+
 bool System::file_exists(const char* file_path) {
 	return std::ifstream(file_path).good();
+}
+
+void System::extract_nibbles(uint16_t instruction, uint8_t(&nibbles)[4]) {
+	nibbles[0] = (instruction & 0xF000) >> 12;
+	nibbles[1] = (instruction & 0x0F00) >> 8;
+	nibbles[2] = (instruction & 0x00F0) >> 4;
+	nibbles[3] = (instruction & 0x000F) >> 0;
+}
+
+void System::x0NNN(uint16_t instruction, bool &done) {
+	if (instruction == 0x00E0) {
+		display->clear_screen();
+	}else if(instruction == 0x00EE) {
+		// subroutine return
+		pc = stack.top();
+		stack.pop();
+	}
+}
+
+void System::x1NNN(uint16_t instruction, bool& done) {
+	pc = instruction & 0x0FFF;
+}
+
+void System::x2NNN(uint16_t instruction, bool& done) {
+	stack.push(pc);
+	pc = instruction & 0x0FFF;
 }
 
 void System::read_rom_to_memory(const char* path_to_rom, uint16_t offset) {
@@ -59,4 +90,37 @@ void System::read_rom_to_memory(const char* path_to_rom, uint16_t offset) {
 	}
 	std::cout << "Wrote " << position << " bytes." << std::endl;
 	rom_file.close();
+	rom_loaded = true;
+}
+
+
+void System::run() {
+	pc = 0x200;
+	if (!rom_loaded)throw std::logic_error("Load ROM before running.");
+	bool done = false;
+	while (!done) {
+		if (pc > 0x0FFF) {
+			done = true;
+			continue;
+		}
+		uint16_t full_instruction = ((uint16_t)read_from_memory(pc) << 8) + read_from_memory(pc + 1);
+		// increment PC by 2
+		pc += 2;
+
+
+		uint8_t nibbles[4];
+		extract_nibbles(full_instruction, nibbles);
+
+		switch (nibbles[0]) {
+		case 0:
+			x0NNN(full_instruction, done);
+			break;
+		case 1:
+			x1NNN(full_instruction, done);
+			break;
+		case 2:
+			x2NNN(full_instruction, done);
+		}
+
+	}
 }
--- chip8/system.h
+++ chip8/system.h
@@ -8,12 +8,12 @@ class System {
 
 	// 4Kb direct access
 	uint8_t* memory;
-	// Points to current instruction in memory
-	void** pc;
+	// current instruction offset in memory
+	uint16_t pc;
 	// 16 bit I register
 	void* i_register;
 	// stack for 16 bit addresses
-	std::stack<void*> stack;
+	std::stack<uint16_t> stack;
 	// 8-bit delay timer
 	uint8_t delay_timer;
 	// 8-bit sound timer
@@ -23,8 +23,19 @@ class System {
 
 	Display* display;
 
+	bool rom_loaded;
+
 	// check if file exists
 	bool file_exists(const char* file_path);
+	// extract 4 nibbles from 16-bit instruction
+	void extract_nibbles(uint16_t instruction, uint8_t (& nibbles)[4]);
+
+	// 0NNN instructions
+	void x0NNN(uint16_t instruction, bool &done);
+	// 1NNN instructions
+	void x1NNN(uint16_t instruction, bool& done);
+	// 2NNN instructions
+	void x2NNN(uint16_t instruction, bool& done);
 
 public:
 	// Default constructor
@@ -33,6 +44,12 @@ public:
 	// Writes a given byte to memory location
 	void write_to_memory(uint16_t address, uint8_t value);
 
+	// Read a byte from given memory location
+	uint8_t read_from_memory(uint16_t address);
+
 	// Read rom
 	void read_rom_to_memory(const char* path_to_rom, uint16_t offset=0x200);
+
+	// Run ROM
+	void run();
 };
